73625144a3542d5bf75a14bf46827c49bf1632d5
sandiseki <sandi.prasekti@gmail.com>
2020-11-04 11:50:59 +0700

    finish

---
+++ app/Views/foot.php
@@ -0,0 +1,4 @@
+<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/adminlte.min.js') ?>"></script>
